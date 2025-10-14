904b034addb118e24b4f12fd28ba6c2a35c78a09
Ebrahem Elmesiry <ebrahemelmesiry@gmail.com>
2025-10-14 12:54:21 +0300

    make about us page

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -1,51 +1,43 @@
-import Image from "next/image";
 import React from "react";
-import CardCategory from "../../../../components/shared/Card/CardCategory";
-import { aboutCard } from "../../../../constant/filterNavbar";
-import HomeIcons from "../../../../components/shared/HomeIcons";
 
 const page = () => {
   return (
-    <div className="flex flex-col gap-10">
-      <div className="grid grid-cols-1 gap-5 items-center md:grid-cols-2 h-[400px]">
-        {/* text About */}
-        <div>
-          <p className="text-[24px] font-medium mb-3">Our Story</p>
-          <div className="flex flex-col text-[13px] gap-2">
-            <span>
-              Launced in 2015, Exlusive is south Asia&apos;s premier online
-              shopping makterplace with an active presense in Bangladesh.
-              Launced in 2015, Exlusive is south Asia&apos;s premier online
-              shopping makterplace with an active presense in Bangladesh.
-            </span>
-            <span>
-              Launced in 2015, Exlusive is south Asia&apos;s premier online
-              shopping makterplace with an active presense in Bangladesh.
-            </span>
-          </div>
-        </div>
-        <div className="relative h-full">
-          <Image src={`/images/Frame 560.webp`} alt="aboutImage" fill />
-        </div>
+    <div className="mx-auto w-[85%]">
+      <p className="text-[35px] font-medium font-inter mb-3 text-center">
+        About Us
+      </p>
+      <div className="flex flex-col text-[16px] text-[#666] gap-3">
+        <span>
+          Welcome to Exclusive, your one-stop-shop for all your computer
+          hardware needs. Our company was founded in [2000] with the goal of
+          providing high-quality computer hardware products at affordable prices
+          to the people of Egypt.
+        </span>
+        <span>
+          At Exclusive, we are committed to providing our customers with the
+          best shopping experience possible. We offer a wide selection of
+          computer hardware products from top brands such as [ASUS], [GIGABYTE],
+          and [SAMSUNG]. Whether you are looking for a new computer case,
+          motherboard, or graphics card, we have everything you need to build
+          your dream PC.
+        </span>
+        <span>
+          Our team of knowledgeable and friendly staff is always ready to help
+          you with any questions you may have. We pride ourselves on our
+          excellent customer service and strive to make every interaction with
+          our customers a positive one.
+        </span>
+        <span>
+          In addition to our top-quality products and customer service, we also
+          offer competitive prices and fast shipping to ensure that our
+          customers receive their orders as quickly and affordably as possible.
+        </span>
+        <span>
+          Thank you for choosing Exclusive as your go-to computer hardware
+          store. We look forward to serving you and helping you build the
+          computer of your dreams.
+        </span>
       </div>
-      <div
-        className="grid gap-2 grid-flow-col
-        max-lg:auto-cols-[calc((100%/5))] 
-        max-md:auto-cols-[calc((100%/4))] 
-        max-sm:auto-cols-[calc((100%/3))]
-        max-400:auto-cols-[calc((100%/2))]"
-      >
-        {aboutCard.map((category) => (
-          <CardCategory
-            type="about"
-            key={category.text}
-            icon={category.icon}
-            text={category.text}
-            value={category.value}
-          />
-        ))}
-      </div>
-      <HomeIcons />
     </div>
   );
 };
