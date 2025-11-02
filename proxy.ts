2ca6745b9c9e7161dc70ddcdf9fd25e9bc96590e
Ebrahem Elmesiry <ebrahemelmesiry@gmail.com>
2025-11-02 12:27:24 +0200

    .

--- proxy.ts
+++ proxy.ts
@@ -7,6 +7,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|log-in|sign-up|contact|$|about|search|productDetails).*)",
+    "/((?!_next/static|_next/image|favicon.ico|log-in|sign-up|contact|$|about|search|productDetails|images).*)",
   ],
 };
