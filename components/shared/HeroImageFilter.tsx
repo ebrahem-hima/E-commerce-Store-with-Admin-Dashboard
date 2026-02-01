292320034938055aa62b551a3f354074fff2c27f
Ebrahem Elmesiry <ebrahemelmesiry@gmail.com>
2026-02-02 00:23:24 +0200

    add hero Image

--- components/shared/HeroImageFilter.tsx
+++ components/shared/HeroImageFilter.tsx
@@ -3,10 +3,14 @@
 import Image from "next/image";
 import Filters from "../Navbar/Filters";
 
-const HeroImageFilter = () => {
+const HeroImageFilter = ({
+  filter,
+}: {
+  filter: { name: string; id: number }[];
+}) => {
   return (
-    <div className="grid gap-10 grid-cols-[300px_1fr] h-[340px] max-lg:grid-cols-1">
-      <Filters />
+    <div className="grid gap-10 grid-cols-[300px_1fr] h-85 max-lg:grid-cols-1">
+      <Filters filter={filter} />
       <div className="relative">
         <Image
           src={`/images/heroImage.webp`}
