4f7b2613c863402c2b9b7e24ca8180607442ef25
Ebrahem Elmesiry <ebrahemelmesiry@gmail.com>
2026-02-02 00:36:08 +0200

    Add wishlist functionality: manage favorite products for users

---
+++ app/(root)/wishlist/wishListClientComponent.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Products from "@/components/shared/Card/ProductCard/Products";
+import { Button } from "@/components/ui/button";
+import { useProductContext } from "@/context/productContext";
+import { moveAllToBag } from "@/lib/userCartFn";
+import { typeProduct } from "@/types/productTypes";
+import Link from "next/link";
+import { useState } from "react";
+
+const WishListClientComponent = ({ data }: { data: typeProduct[] }) => {
+  const { setCartData, cartData } = useProductContext();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const productsWishList = data.map(({ id, ...rest }) => ({
+    ...rest,
+    id: rest.product_id || "",
+  }));
+
+  const [wishList, setWishList] = useState(productsWishList);
+  return (
+    <>
+      <div className="flex-between my-4">
+        <span className="font-medium">Wishlist ({wishList.length})</span>
+        <Button
+          variant={"white"}
+          size={"default"}
+          disabled={wishList.length > 0}
+          onClick={() =>
+            moveAllToBag({
+              wishList,
+              setCartData,
+              cartData,
+            })
+          }
+        >
+          Move All to Bag
+        </Button>
+      </div>
+      {wishList.length > 0 ? (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+          <Products data={wishList} setData={setWishList} />
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center py-20">
+          <span className="text-4xl mb-4">💔</span>
+          <h2 className="text-xl font-semibold mb-2">Your wishlist is empty</h2>
+          <p className="text-gray-500 mb-6">
+            Start adding your favorite products to see them here.
+          </p>
+          <Link
+            href={`/`}
+            className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-hover cursor-pointer duration-300"
+          >
+            Shop Now
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default WishListClientComponent;
