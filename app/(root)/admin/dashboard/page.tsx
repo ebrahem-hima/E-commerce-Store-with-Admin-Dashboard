1de0b42b1fbaa847684e14b51a497baef3ebcf53
Ebrahem Elmesiry <ebrahemelmesiry@gmail.com>
2026-02-04 22:19:02 +0200

    Fix loaders issue, add dashboard page title

--- app/(root)/admin/dashboard/page.tsx
+++ app/(root)/admin/dashboard/page.tsx
@@ -1,50 +1,15 @@
-import CustomTable from "@/components/shared/table/customTable";
-import Info from "./Info";
-import { headTopMember, headTopProduct } from "@/constant/table";
-import { getDashboardData } from "./services/dashboardService";
+import { Suspense } from "react";
+import DashboardContent from "./components/DashboardContent";
+import LoadingSpinner from "@/components/shared/LoadingSpinner";
+import PageHeader from "../shared/PageHeader";
 const Page = async () => {
-  const {
-    order_count,
-    order_total,
-    customers_count,
-    top_customers,
-    top_products,
-  } = await getDashboardData();
-
   return (
-    <div className="flex flex-col gap-6">
-      {/* Info */}
-      <div className="w-full overflow-x-auto">
-        <div className="flex min-w-112.5 gap-4">
-          <Info type="total" text="Total Reven" number={order_total} />
-          <Info type="order" text="Orders" number={order_count} />
-          <Info
-            type="user"
-            text="Existing Users"
-            number={customers_count || 0}
-          />
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="font-medium">
-          <p>Top Member by Units Sold</p>
-          <CustomTable
-            empty_table="No users have made purchases yet."
-            dataBody={top_customers}
-            titles={headTopMember}
-          />
-        </div>
-        <div className="font-medium">
-          <p>Top Products by Units Sold</p>
-          <CustomTable
-            empty_table="No products have been sold yet."
-            dataBody={top_products}
-            titles={headTopProduct}
-          />
-        </div>
-      </div>
-    </div>
+    <>
+      <PageHeader title="Dashboard" />
+      <Suspense fallback={<LoadingSpinner />}>
+        <DashboardContent />
+      </Suspense>
+    </>
   );
 };
 
--- app/(root)/admin/orders/page.tsx
+++ app/(root)/admin/orders/page.tsx
@@ -1,35 +1,22 @@
 import PageHeader from "../shared/PageHeader";
 import { Suspense } from "react";
 import LoadingSpinner from "@/components/shared/LoadingSpinner";
-import TableSearch from "../shared/TableSearch";
-import { getOrders } from "./orderService";
-import {
-  orderSortOptions,
-  orderTableColumns,
-} from "@/constant/admin/admin-tables/order_table";
+import OrderTable from "./orderTable";
 
 interface Props {
-  searchParams: {
+  searchParams: Promise<{
     filter: string;
     search: string;
-  };
+  }>;
 }
 
 const Page = async ({ searchParams }: Props) => {
   const getSearchParams = await searchParams;
-  const data = await getOrders(getSearchParams);
   return (
     <>
       <PageHeader title="Orders" />
       <Suspense fallback={<LoadingSpinner />}>
-        <TableSearch
-          tableWidth="min-w-[630px]"
-          typeTable="user_order"
-          selectOptions={orderSortOptions}
-          tableData={data || []}
-          headers={orderTableColumns}
-          emptyMessage="There are no orders to display"
-        />
+        <OrderTable getSearchParams={getSearchParams} />
       </Suspense>
     </>
   );
