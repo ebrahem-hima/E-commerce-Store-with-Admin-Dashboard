36d64b06e9d7d0f75c190e6760d940c0428273f0
Ebrahem Elmesiry <ebrahemelmesiry@gmail.com>
2026-04-10 22:19:26 +0200

    Remove searchPageContent because it is unnecessary Show a message when there are no products with or without discount

--- app/(root)/search/components/SliderComponentContent.tsx
+++ app/(root)/search/components/SliderComponentContent.tsx
@@ -30,7 +30,9 @@ const SliderComponentContent = async ({
           />
         )}
         {/* Products + Sort by */}
-        {otherProducts.length === 0 && <p>No products found</p>}
+        {bestSellersProducts.length === 0 && otherProducts.length === 0 && (
+          <p>No products found</p>
+        )}
         {otherProducts.length > 0 && <SearchProduct products={otherProducts} />}
       </div>
       <SearchFilterMobile filter={mergeArray} />
